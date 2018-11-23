e4a725c3e71da857716f148cc7ffcb0563d3b292
Srinath V <srinathv1001@gmail.com>
2018-11-24 01:08:23 +0530

    organized

--- faceRecognition.py
+++ faceRecognition.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def faceDetection(test_img):
     gray_img = cv2.cvtColor(test_img,cv2.COLOR_BGR2GRAY)
-    face_haar_cascade = cv2.CascadeClassifier('/Users/rohit/Desktop/FaceRecog/HaarCascade/haarcascade_frontalface_default.xml')
+    face_haar_cascade = cv2.CascadeClassifier('C:/Users/Srinath/Desktop/FaceRecog/HaarCascade/haarcascade_frontalface_default.xml')
     faces = face_haar_cascade.detectMultiScale(gray_img,scaleFactor = 1.32,minNeighbors=5)
     return faces,gray_img
 
--- tester.py
+++ tester.py
@@ -4,39 +4,58 @@ import numpy as np
 import faceRecognition as fr
 
 
+## TRAINING
+
+#faces,faceID=fr.labels_for_training_data('C:/Users/Srinath/Desktop/FaceRecog/trainingImages')
+#face_recognizer=fr.train_classifier(faces,faceID)
+#face_recognizer.write('trainingData.yml')
+#================================= END OF TRAINING
+# Uncomment the above lines when training a face.
+
+
+## TESTING
+
+face_recognizer=cv2.face.LBPHFaceRecognizer_create()
+face_recognizer.read('trainingData.yml')
+name={0:"Chris Evans",1:"Robert Downey Jr.", 2: "Tom Hollands"}
+
+
+
 #This module takes images  stored in diskand performs face recognition
-test_img=cv2.imread('/Users/rohit/Desktop/FaceRecog/TestImages/test.jpg')#test_img path
-faces_detected,gray_img=fr.faceDetection(test_img)
-print("faces_detected:",faces_detected)
 
 
-#Uncomment belows lines when running this program first time.Since it svaes training.yml file in directory
-faces,faceID=fr.labels_for_training_data('/Users/rohit/Desktop/FaceRecog/trainingImages')
-face_recognizer=fr.train_classifier(faces,faceID)
-face_recognizer.save('trainingData.yml')
-#face_recognizer=cv2.face.LBPHFaceRecognizer_create()
-#face_recognizer.read('trainingData.yml')#use this to load training data for subsequent runs
+## TESTING
 
-name={0:"Rohit",1:"Taylor"}#creating dictionary containing names for each label
+directory = "C:/Users/Srinath/Desktop/FaceRecog/TestImages"
 
-for face in faces_detected:
-    (x,y,w,h)=face
-    roi_gray=gray_img[y:y+h,x:x+h]
-    label,confidence=face_recognizer.predict(roi_gray)#predicting the label of given image
-    print("confidence:",confidence)
-    print("label:",label)
-    fr.draw_rect(test_img,face)
-    predicted_name=name[label]
-    if(confidence>37):#If confidence greater than 37 then don't print predicted face text on screen
-        continue
-    fr.put_text(test_img,predicted_name,x,y)
+for path,subdirnames,filenames in os.walk(directory):
+	for filename in filenames:
+		if filename.startswith("."):
+			print("Skipping system file")
+			continue
+		id=os.path.basename(path)#fetching subdirectory names
+		img_path=os.path.join(path,filename)
+		print("img_path: ",img_path+"\n")
+		#print("id:",id)
+		test_img=cv2.imread(img_path)
+		if test_img is None:
+			print("Image Not Loaded Properly!")
+			continue
+		faces_detected,gray_img=fr.faceDetection(test_img)
+		for face in faces_detected:
+			(x,y,w,h)=face
+			roi_gray=gray_img[y:y+h,x:x+h]
+			face_recognizer.read('trainingData.yml')
+			label,confidence=face_recognizer.predict(roi_gray)#predicting the label of given image
+			#print("confidence:",confidence)
+			fr.draw_rect(test_img,face)
+			predicted_name=name[label]
+			if(confidence>37):#If confidence greater than 37 then don't print predicted face text on screen
+				print("\n==================")
+				continue
+			#fr.put_text(test_img,predicted_name,x,y)
+			#resized_img=cv2.resize(test_img,(600,400))
+			# not displaying... just printing if recognized or not.
+			print("recognized " + predicted_name +" in "+img_path+"\n==================")
 
-resized_img=cv2.resize(test_img,(600,400))
-cv2.imshow("Face Recog",resized_img)
-cv2.waitKey(0)#Waits indefinitely until a key is pressed
-cv2.destroyAllWindows
-
-
-
-
-
+## END OF TESTING
