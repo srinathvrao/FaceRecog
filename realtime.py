2a339b8182b4613b9e98e2714f7627334de51000
Sainath Prasanna <sainathprasanna@gmail.com>
2019-07-13 10:21:20 +0100

    add realtime.py

--- realtime.py
+++ realtime.py
@@ -1,17 +1,7 @@
 import dlib
-import cv2
-from sklearn.svm import SVC
-import dlib
 import numpy as np
-from sklearn.svm import LinearSVC
-from sklearn.metrics import accuracy_score, recall_score, precision_score
-from sklearn.metrics import confusion_matrix
-from sklearn.decomposition import PCA
-import pickle
-import matplotlib.pyplot as plt
-from sklearn.utils.multiclass import unique_labels
 from time import sleep
-
+from PIL import Image
 face_detector = dlib.get_frontal_face_detector()
 pose_predictor_68_point = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 face_encoder = dlib.face_recognition_model_v1('dlib_face_recognition_resnet_model_v1.dat')
@@ -27,37 +17,31 @@ def whirldata_face_encodings(face_image,num_jitters=1):
 	except Exception as e:
 		return []
 
-filename = 'svm.sav'
-in_file = open(filename, 'rb')
-clf = pickle.load(in_file)
-import cv2
-cap = cv2.VideoCapture(0)
-l=[]
-while(True):
-	ret, frame = cap.read()
-	frame = cv2.flip(frame,1)
-	# Our operations on the frame come here
-	# print(whirldata_face_detectors(gray))
-	# detections = whirldata_face_detectors(gray)
-	# for detection in detections:
-	# 	x = (detection.left()) # x
-	# 	y = (detection.top()) # y
-	# 	w = (detection.right() - x) # width 
-	# 	h = (detection.bottom() - y) # height
-	# 	# single_op = clf.predict(test_np)
-	repre = whirldata_face_encodings(frame)
-	test_op = clf.predict(np.array([repre]))
-	print(test_op)
-		# break
-		# cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+with Image.open('2.jpg') as img:
+	#img = img.convert('LA')
+	np_img = np.array(img)
+	#print(np
+	print(np_img.shape)
+	repre = whirldata_face_encodings(np_img)
+	print(repre)
 
-	# Display the resulting frame
-	cv2.imshow('frame',frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-# with open('sainath.txt','wb') as file:
-# 	pickle.dump(l,file)
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+'''
+cmake --build . --config Release
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+            -D CMAKE_INSTALL_PREFIX=/usr/local \
+            -D INSTALL_C_EXAMPLES=ON \
+            -D INSTALL_PYTHON_EXAMPLES=ON \
+            -D WITH_TBB=ON \
+            -D WITH_V4L=ON \
+            -D OPENCV_PYTHON3_INSTALL_PATH=/usr/lib/python2.7/dist-packages \
+        -D WITH_QT=ON \
+        -D WITH_OPENGL=ON \
+        -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+        -D BUILD_EXAMPLES=ON ..
+sudo make install
+sudo ldconfig
+cd ..
+
+        '''
