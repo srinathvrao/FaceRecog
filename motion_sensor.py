a6245405204728204d0425c511bf7531d677f88e
Sainath Prasanna <sainathprasanna@gmail.com>
2019-07-13 12:12:54 +0100

    working motion sensor takes picture on motion

--- motion_sensor.py
+++ motion_sensor.py
@@ -1,10 +1,26 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setwarnings(False)
+from picamera import PiCamera
+'''GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
 GPIO.setup(3, GPIO.OUT)         #LED output pin
+print('works')
 while True:
 	i=GPIO.input(11)
 	if i==0:                 #When output from motion sensor is LOW
-		print("No intruders",i)
+		print("No intruders")
+	else:
+                print("Intruders") '''
+from gpiozero import MotionSensor
+camera = PiCamera()
+pir = MotionSensor(4)
+#while True:
+start_time = time.time()
+pir.wait_for_motion()
+print('motion');
+camera.capture('test.png')
+pir.wait_for_no_motion()
+camera.close()
+print('Motion');
+print(time.time() - start_time)
