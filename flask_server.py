7f14b691bb0c3d93f2346bc4033f5a365618b7cb
Sainath Prasanna <sainathprasanna@gmail.com>
2019-08-25 14:42:40 +0530

    get image, time and camera id as json

--- flask_server.py
+++ flask_server.py
@@ -84,13 +84,13 @@ def calculateAttendance(label, in_time, out_time):
 def calTimeDelta(now_time, out_time):
 	td = now_time - out_time
 	mins = (td.seconds//60)%60
-	if mins > 2
+	if mins > 2:
 		return True
 	return False
 
 @app.route("/image", methods=['POST'])
 def sendResult():
-	calculateAttendance('3_cse_c',"08:20","12:40")
+	#calculateAttendance('3_cse_c',"08:20","12:40")
 
 	# print("got an image")
 	# print("HELLOOOOO \n\n")
@@ -98,7 +98,8 @@ def sendResult():
 	# dic = request.data
 	# bytes = readimage(dic)
 	# image = Image.open(io.BytesIO(bytes))
-	dic = request.data
+	##dic = request.data
+	dic = request.json
 	print(type(dic))
 	print()
 	#arr = np.array(dic['arr'])
@@ -106,9 +107,9 @@ def sendResult():
 	#bytes = readimage(dic)
 	#bytes = io.BytesIO(dic)
 	print()
-	#print(dic['time'])
+	print(dic['time'])
 	print()
-	picnp = np.fromstring(dic, dtype=np.uint8)
+	picnp = np.fromstring(base64.b64decode(dic['img']), dtype=np.uint8)
 	#image = Image.open(io.BytesIO(dic))
 	#print(type(bytes))
 	#print(type(picnp))
@@ -159,24 +160,26 @@ def sendResult():
 							print(str(label),"recognized entering, marked present at",dt_str)
 							mongo.db.attendance.update_one(myquery,newvalues)
 						elif doc['present'] == "True" and calTimeDelta(datetime.datetime.now() ,datetime.datetime.strptime(doc['out'],"%H:%M")):
+							print('penalize')
 							## TODO: Penalize
-						else:
-							print("ALREADY MARKED")
-			else: # camera 2 triggered (going out)
-				for label in test_op:
-					docs = mongo.db.attendance.distinct("cse_c_"+str(label))
-					for doc in docs:
-						if doc['present']=="True": #and calTimeDelta(datetime.datetime.now() ,datetime.datetime.strptime(doc['out'],"%H:%M")) == False:
-							myquery = {"cse_c_"+str(label) : doc}
-							doc2 = {}
-							doc2['out'] = datetime.datetime.now().strftime("%H:%M")
-							doc2['present'] = "False"
-							newvalues = { "$set": {"cse_c_"+str(label) : doc2} }
-							print(str(label),"recognized leaving, marked absent")
-							mongo.db.attendance.update_one(myquery,newvalues)
+						#else:
+						#	print("ALREADY MARKED")
+				else: # camera 2 triggered (going out)
+					for label in test_op:
+						docs = mongo.db.attendance.distinct("cse_c_"+str(label))
+						for doc in docs:
+							if doc['present']=="True": #and calTimeDelta(datetime.datetime.now() ,datetime.datetime.strptime(doc['out'],"%H:%M")) == False:
+								myquery = {"cse_c_"+str(label) : doc}
+								doc2 = {}
+								doc2['out'] = datetime.datetime.now().strftime("%H:%M")
+								doc2['present'] = "False"
+								newvalues = { "$set": {"cse_c_"+str(label) : doc2} }
+								print(str(label),"recognized leaving, marked absent")
+								mongo.db.attendance.update_one(myquery,newvalues)
 
-						elif doc['present'] == "False" and calTimeDelta(datetime.datetime.now() ,datetime.datetime.strptime(doc['out'],"%H:%M")):
-							## TODO: Penalize
+							elif doc['present'] == "False" and calTimeDelta(datetime.datetime.now() ,datetime.datetime.strptime(doc['out'],"%H:%M")):
+								print('penalize')
+								## TODO: Penalize
 
 
 		else:
@@ -193,5 +196,5 @@ if __name__ == "__main__":
 	print(1,"midha")
 	print(2,"srinath")
 	#app.run("192.168.1.6",port=8083)
-	http_server = WSGIServer(('127.0.0.1', 8083), app)
+	http_server = WSGIServer(('192.168.137.1', 8083), app)
 	http_server.serve_forever()
